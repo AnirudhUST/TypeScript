c9096bf7fb24ca8b4f593813a02ca69699490192
Anirudh G <Anirudh.G@ust.com>
2023-03-30 15:28:24 +0530

    next commit

---
+++ inheritance.ts
@@ -0,0 +1,73 @@
+/* Create a TypeScript program to implement a class hierarchy for different types of animals. Each animal should have a name, age, and sound that it makes. 
+Use inheritance to create subclasses for different types of animals such as mammals, birds, and fish.*/
+
+ 
+
+
+class Animal {
+    name: string;
+    age: number;
+    sound: string;
+  
+    constructor(name: string, age: number, sound: string) {
+      this.name = name;
+      this.age = age;
+      this.sound = sound;
+    }
+  
+    makeSound() {
+      console.log(this.sound);
+    }
+  }
+  
+  class Mammal extends Animal {
+    numberOfLegs: number;
+  
+    constructor(name: string, age: number, sound: string, numberOfLegs: number) {
+      super(name, age, sound);
+      this.numberOfLegs = numberOfLegs;
+    }
+  
+    giveBirth() {
+      console.log(`${this.name} gives birth to live young.`);
+    }
+  }
+  
+  class Bird extends Animal {
+    wingspan: number;
+  
+    constructor(name: string, age: number, sound: string, wingspan: number) {
+      super(name, age, sound);
+      this.wingspan = wingspan;
+    }
+  
+    fly() {
+      console.log(`${this.name} takes off into the sky!`);
+    }
+  }
+  
+  class Fish extends Animal {
+    habitat: string;
+  
+    constructor(name: string, age: number, sound: string, habitat: string) {
+      super(name, age, sound);
+      this.habitat = habitat;
+    }
+  
+    swim() {
+      console.log(`${this.name} swims through the ${this.habitat}.`);
+    }
+  }
+  
+  const dog = new Mammal("Buddy", 5, "woof", 4);
+  dog.makeSound(); 
+  dog.giveBirth(); 
+  
+  const eagle = new Bird("Aquila", 3, "screech", 72);
+  eagle.makeSound(); 
+  eagle.fly(); 
+  
+  const salmon = new Fish("Sammy", 2, "blub", "river");
+  salmon.makeSound(); 
+  salmon.swim();
+  
